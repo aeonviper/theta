aa4ac5659703fe0d6ecc87703f83d9a565192e09
aeonviper <viperblaster@gmail.com>
2022-01-28 07:21:51 +0700

    Code

--- src/theta/console/PasswordHasher.java
+++ src/theta/console/PasswordHasher.java
@@ -6,7 +6,7 @@ import theta.core.Utility;
 
 public class PasswordHasher {
 
-	public static void main(String[] args) {
+	public static void main(String[] array) {
 		System.out.println(Utility.hashPassword(new Scanner(System.in).nextLine()));
 	}
 
